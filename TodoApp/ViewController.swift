81bd03c1bc2abe81a37de0536f7e99c7da362b53
TaToonz <peerapath.t@gmail.com>
2019-07-20 17:52:25 +0700

    binding model to view with controller

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    var todo = Todo()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo.totalItems
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        let item = todo.item(at: indexPath.row)
+        
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.isDone ? .checkmark : .none
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        todo.add(item: TodoItem(title: "Download XCode", isDone: true))
+        todo.add(item: TodoItem(title: "Buy milk"))
+        todo.add(item: TodoItem(title: "Learning Swift"))
     }
 
 
